9431f8314ef9a97a937d2bcf8bdf17f2bc086435
Magali-e <52261091+magali-e@users.noreply.github.com>
2019-10-10 12:06:23 -0500

    Update Kmeans_M4D_separar.py

--- Kmeans_M4D_separar.py
+++ Kmeans_M4D_separar.py
@@ -78,7 +78,7 @@ def fit_samples(samples,num,nombre):
     ax.set_ylabel('Speed (m/s)')     
     grp = kmeans.predict(samples)
     tam=len(samples)
-    print tam,"tam"
+
     cad=[]
     for i in range(tam):
         cad.append(0)
@@ -125,6 +125,8 @@ def fit_samples(samples,num,nombre):
     plt.savefig(nombre,dpi = 300, format="png")
     plt.show()
     plt.clf()
+    plt.close()
+    return 0
 if __name__ == '__main__':
     s = sample()
     componentes=6
